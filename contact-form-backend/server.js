263c950f39bbebfa41491e0326040c96610cd150
mnlazs <mnl.azs@gmail.com>
2024-06-06 16:59:51 -0500

    updated

--- contact-form-backend/server.js
+++ contact-form-backend/server.js
@@ -1,3 +1,6 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: '../.env' }); 
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const nodemailer = require('nodemailer');
@@ -8,6 +11,11 @@ const PORT = process.env.PORT || 3000;
 // Middleware para parsear el cuerpo de la solicitud
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+// Ruta raíz para probar que el servidor está activo
+app.get('/', (req, res) => {
+    res.send('Bienvenido a mi servidor de correo!');
+});
 
 // Configurar CORS (opcional, si es necesario)
 app.use((req, res, next) => {
@@ -25,27 +33,25 @@ app.post('/send-email',async  (req, res) => {
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: 'mnl.azs@gmail.com',
-            pass: 'BatmanyR',
+            user: process.env.EMAIL_USER,
+            pass: process.env.EMAIL_PASS,
         },
     });
 
     const mailOptions = {
         from: email,
-        to: 'destinatario@example.com',
+        to: 'mnl.azs@gmail.com',
         subject: `Mensaje de ${name}`,
         text: message,
     };
 
     try {
         const info = await transporter.sendMail(mailOptions);
-        res.status(200).json({ success: true, message: 'Correo enviado correctamente', info });
+        console.log('Message sent: %s', info.messageId);
+        return res.status(200).json({ success: true, message: 'Correo enviado correctamente', info });
     } catch (error) {
         console.error('Error al enviar el correo:', error);
-        if (error.response) {
-            console.error('Response:', error.response.body);
-        }
-        res.status(500).json({ success: false, message: 'Error al enviar el correo', error });
+        return res.status(500).json({ success: false, message: 'Error al enviar el correo', error: error.message });
     }
 });
 
